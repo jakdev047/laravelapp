e86fa25c2685a172c58a187bd682e8fc95f406cf
jakdev047 <jakdevfreelancer047@gmail.com>
2021-04-21 12:48:50 +0600

    make controller

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// admin
 
+// Route::get('/admin', function () {
+//     return view('admin.index');
+// });
+
+Route::get('/admin',[AdminController::class,'index']);
+
+//  laravel welcome
 Route::get('/', function () {
     return view('welcome');
-});
-
-// admin
-Route::get('/admin', function () {
-    return view('admin.index');
 });
